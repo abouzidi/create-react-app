bdce53a9263e5a841685eda4c203cd389906aeb2
Nicolas Gotchac <ngotchac@gmail.com>
2017-12-21 18:08:11 +0100

    Add status text to Wepback HMR

--- packages/react-scripts/config/app.index.js
+++ packages/react-scripts/config/app.index.js
@@ -28,6 +28,81 @@ ReactDOM.render(
 );
 
 if (module.hot) {
+  const socket = require('webpack-dev-server/client/socket');
+
+  /** Show Webpack HMR status */
+  function hide() {
+    const id = 'parity-react-scripts--hmr-status';
+    const div = document.getElementById(id);
+
+    if (!div) {
+      return;
+    }
+
+    document.body.removeChild(div);
+  }
+
+  function status(message, { timeout, error } = {}) {
+    const id = 'parity-react-scripts--hmr-status';
+    const div = document.getElementById(id) || document.createElement('div');
+
+    div.id = id;
+    div.innerText = message;
+    div.style = `
+      position: absolute;
+      top: 0;
+      padding: 7px 10px;
+      background: #343d46;
+      color: #eff1f5;
+      font-size: 12px;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      width: 100%;
+      box-sizing: border-box;
+      cursor: pointer;
+      border-bottom: 1px solid white;
+      border-left: 10px solid ${error ? '#ebcb8b' : '#a3be8c'};
+    `;
+
+    div.onclick = () => {
+      hide();
+    };
+
+    if (timeout) {
+      setTimeout(() => {
+        hide();
+      }, timeout);
+    }
+
+    document.body.append(div);
+  }
+
+  // Listen to Webpack
+  socket('/sockjs-node', {
+    invalid: () => {
+      status('App updated. Recompiling...');
+    },
+    'still-ok': () => {
+      status('App ready.', { timeout: 2000 });
+    },
+    ok: () => {
+      status('App ready.', { timeout: 2000 });
+    },
+    warnings: () => {
+      status('Warnings while compiling.');
+    },
+    errors: errors => {
+      status('Errors while compiling.\n' + errors.join('\n\n'), {
+        error: true,
+      });
+    },
+    close: () => {
+      status(
+        'Lost connection to webpack-dev-server.\n  Please restart the server to re-establish connection...',
+        { error: true }
+      );
+    },
+  });
+
   module.hot.accept('Application', () => {
     const NextApplication = require('Application').default;
 
